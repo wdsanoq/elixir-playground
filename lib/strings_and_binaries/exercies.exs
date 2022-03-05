5410b73d69205a3491f57c7b60125b359c7b64a9
Nobody <74131447+wdsanoq@users.noreply.github.com>
2022-03-05 23:26:28 +0300

    Strings 2

--- lib/strings_and_binaries/exercies.exs
+++ lib/strings_and_binaries/exercies.exs
@@ -1,5 +1,8 @@
 defmodule MyString do
   def printable_ascii(s), do: Enum.all?(s, fn ch -> ch in ?\s..?~ end)
+
+  def anagram?(word1, word2), do: Enum.sort(word1) == Enum.sort(word2)
 end
 
 IO.inspect(MyString.printable_ascii('test!'))
+IO.inspect(MyString.anagram?('cat', 'act'))
